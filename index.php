2475395e72ff591ab566c2bb8ae321635b1f76cd
Glen Leao <glen@milanezmilaneze.local>
2019-12-18 15:49:11 -0300

    projeto dash

---
+++ index.php
@@ -0,0 +1,316 @@
+<!DOCTYPE html>
+<html lang="pt">
+
+<head>
+
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+
+  <title>Milanez & Milaneze - Dashboard</title>
+
+  <!-- Custom fonts for this template-->
+  <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700&display=swap" rel="stylesheet">
+
+  <!-- Custom styles for this template-->
+  <link href="css/sb-admin-2.min.css" rel="stylesheet">
+  
+  <style>
+    body:fullscreen{width: 100%;height: 100%;}
+    h1 {
+      font-size: 1.5rem;
+    }
+    .text-vsf{color: #964097;}
+    .text-csf{color: #40BDED;}
+    .text-mec{color: #61C086;}
+    .text-expo{color: #A3CC3F;}
+    .text-wine{color: #CFDD26;}
+
+    .p-b-vsf{background-color: #964097;}
+    .p-b-csf{background-color: #40BDED;}
+    .p-b-mec{background-color: #61C086;}
+    .p-b-expo{background-color: #A3CC3F;}
+    .p-b-wine{background-color: #CFDD26;}
+    
+
+  </style>    
+</head>
+
+
+<body id="page-top" style="background: red;" >
+
+  <!-- Page Wrapper -->
+  <div id="wrapper">
+
+
+    <!-- Content Wrapper -->
+    <div id="content-wrapper" class="d-flex flex-column">
+
+      <!-- Main Content -->
+      <div id="content">
+
+        <!-- Begin Page Content -->
+        <div class="container-fluid">
+
+          <!-- Page Heading -->
+          <div class="d-sm-flex align-items-center justify-content-between my-2">
+            <h1 class="h3 mb-0 text-gray-800">OUTUBRO</h1>
+          </div>
+
+          <!-- Content Row -->
+          <div class="row">
+
+            <div class="col-xl-8 mb-4">
+              <!-- VSF  -->
+              <div class="row">
+
+
+              <div class="col-xl-6 col-md-12 mb-4">
+                <div class="card h-100 shadow py-2">
+                  <div class="card-body">
+                    <div class="row no-gutters align-items-center">
+                      <div class="col mr-2">
+                        <div class="h6 font-weight-bold text-uppercase mb-1">vitoria stone fair</div>
+                        <div class="h3 mb-0 font-weight-bold text-gray-800">38%</div>
+                         <div class="progress progress-sm mr-2" style="height:15px;">
+                              <div class="progress-bar p-b-vsf bg-bar" role="progressbar" style="width: 38%" aria-valuenow="38" aria-valuemin="0" aria-valuemax="100"></div>
+                          </div>
+                        <div class="h5 text-center mt-3">45%</div>
+                      </div>
+                      <div class="col-auto">
+                        <i class="fas fa-caret-up fa-3x text-success"></i>
+                        <!-- <i class="fas fa-calendar fa-2x text-gray-300"></i> -->
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+              <!-- CSF -->
+              <div class="col-xl-6 col-md-12 mb-4">
+                <div class="card h-100 shadow py-2">
+                  <div class="card-body">
+                    <div class="row no-gutters align-items-center">
+                      <div class="col mr-2">
+                        <div class="h6 font-weight-bold text-secondary text-uppercase mb-1">cachoeiro stone fair</div>
+
+                        <div class="h3 mb-0 font-weight-bold text-gray-800">19%</div>
+                          <div class="progress progress-sm mr-2" style="height:15px;">
+                              <div class="progress-bar p-b-csf bg-bar" role="progressbar" style="width: 19%" aria-valuenow="19" aria-valuemin="0" aria-valuemax="100"></div>
+                          </div>
+
+                        <div>32%</div>
+                      </div>
+
+                      <div class="col-auto">
+                        <i class="fas fa-caret-down fa-3x text-danger"></i>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+              <!-- MEC SHOW Card Example -->
+              <div class="col-xl-6 col-md-12 mb-4">
+                <div class="card h-100 shadow py-2">
+                  <div class="card-body">
+
+                    <div class="row no-gutters align-items-center">
+                      <div class="col mr-2">
+                        <div class="h6 font-weight-bold text-dark text-uppercase mb-1">mec show</div>
+        
+                          <div class="h3 mb-0 font-weight-bold text-gray-800">17%</div>
+                          <div class="progress progress-sm mr-2" style="height:15px;">
+                            <div class="progress-bar p-b-mec bg-bar" role="progressbar" style="width: 17%" aria-valuenow="17" aria-valuemin="0" aria-valuemax="100"></div>
+                          </div>
+                          <div>12%</div> 
+                      </div>
+                    </div>
+                    <div class="col-auto">
+                      <i class="fas fa-caret-down fa-3x text-danger"></i>
+                      <!--  <i class="fas fa-clipboard-list fa-2x text-gray-300"></i> -->
+                    </div>
+                  </div>
+
+                </div>
+              </div>
+
+              <!-- EXPO CONSTRUCOES Card Example -->
+              <div class="col-xl-6 col-md-12 mb-4">
+                <div class="card h-100 shadow py-2">
+                  <div class="card-body">
+                    <div class="row no-gutters align-items-center">
+                      <div class="col mr-2">
+                        <div class="h6 font-weight-bold text-secondary text-uppercase mb-1">expo construções</div>
+
+                        <div class="h3 mb-0 font-weight-bold text-gray-800">4%</div>
+                          <div class="progress progress-sm mr-2" style="height:15px;">
+                              <div class="progress-bar p-b-expo bg-bar" role="progressbar" style="width: 4%" aria-valuenow="4" aria-valuemin="0" aria-valuemax="100"></div>
+                          </div>
+
+                        <div>2%</div>
+                      </div>
+
+                      <div class="col-auto">
+                        <i class="fas fa-caret-down fa-3x text-danger"></i>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+              <!-- WINE Card Example -->
+             
+              <div class="col-xl-6 col-md-12 mb-4">
+                <div class="card h-100 shadow py-2">
+                  <div class="card-body">
+                    <div class="row no-gutters align-items-center">
+                      <div class="col mr-2">
+                        <div class="h6 font-weight-bold text-secondary text-uppercase mb-1">wine south america</div>
+
+                        <div class="h3 mb-0 font-weight-bold text-gray-800">22%</div>
+                          <div class="progress progress-sm mr-2" style="height:15px;">
+                              <div class="progress-bar p-b-wine bg-bar" role="progressbar" style="width: 22%" aria-valuenow="22" aria-valuemin="0" aria-valuemax="100"></div>
+                          </div>
+
+                        <div>12%</div>
+                      </div>
+
+                      <div class="col-auto">
+                        <i class="fas fa-caret-down fa-3x text-danger"></i>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <!--  -->
+
+              </div>
+
+            </div>
+         
+            <!-- Pie Chart -->
+            <div class="col-xl-4 col-lg-5">
+              <div class="card shadow mb-4">
+                <!-- Card Header - Dropdown -->
+                <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
+                  <h6 class="h6 m-0 font-weight-bold text-dark text-uppercase">total</h6>
+                  <div class="dropdown no-arrow">
+                    <a class="dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                      <i class="fas fa-ellipsis-v fa-sm fa-fw text-gray-400"></i>
+                    </a>
+                    <div class="dropdown-menu dropdown-menu-right shadow animated--fade-in" aria-labelledby="dropdownMenuLink">
+                      <div class="dropdown-header">Dropdown Header:</div>
+                      <a class="dropdown-item" href="#">Action</a>
+                      <a class="dropdown-item" href="#">Another action</a>
+                      <div class="dropdown-divider"></div>
+                      <a class="dropdown-item" href="#">Something else here</a>
+                    </div>
+                  </div>
+                </div>
+                <!-- Card Body -->
+                <div class="card-body">
+                  <div class="chart-pie pt-4 pb-2">
+                    <canvas id="myPieChart"></canvas>
+                  </div>
+                  <div class="mt-4 small">
+                    <ul style="list-style-type: none;">
+                    <li class="mb-2">
+                      38% - <i class="fas fa-circle text-vsf"></i> Vitoria Stone Fair
+                    </li>
+                    <li class="mb-2">
+                      19% - <i class="fas fa-circle text-csf"></i> Cachoeiro Stone Fair
+                    </li>
+                   <li class="mb-2">
+                      17% - <i class="fas fa-circle text-mec"></i> Mec Show
+                    </li>
+                   <li class="mb-2">
+                      04% - <i class="fas fa-circle text-expo"></i> Expo Construções
+                    </li>
+                   <li class="mb-2">
+                      22% - <i class="fas fa-circle text-wine"></i> Wine South America
+                    </li>
+                  </ul>
+            
+
+                   
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <!-- Content Row -->
+
+        </div>
+        <!-- /.container-fluid -->
+
+      </div>
+      <!-- End of Main Content -->
+
+    </div>
+    <!-- End of Content Wrapper -->
+
+  </div>
+  <!-- End of Page Wrapper -->
+
+
+
+  <!-- Bootstrap core JavaScript-->
+  <script src="vendor/jquery/jquery.min.js"></script>
+  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+  <!-- Core plugin JavaScript-->
+  <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+
+  <!-- Custom scripts for all pages-->
+  <script src="js/sb-admin-2.min.js"></script>
+
+  <!-- Page level plugins -->
+  <script src="vendor/chart.js/Chart.min.js"></script>
+
+  <!-- Page level custom scripts -->
+  <script src="js/demo/chart-area-demo.js"></script>
+  <script src="js/demo/chart-pie-gln.js"></script>
+  <script type="text/javascript">
+   // progressbar.js@1.0.0 version is used
+  // Docs: http://progressbarjs.readthedocs.org/en/1.0.0/
+
+var bar = new ProgressBar.Line(progressbar, {
+  strokeWidth: 4,
+  easing: 'easeInOut',
+  duration: 1400,
+  color: '#FFEA82',
+  trailColor: '#eee',
+  trailWidth: 10,
+  svgStyle: {width: '100%', height: '100%'},
+  text: {
+    style: {
+      // Text color.
+      // Default: same as stroke color (options.color)
+      color: '#999',
+      position: 'absolute',
+      right: '0',
+      top: '30px',
+      padding: 0,
+      margin: 0,
+      transform: null
+    },
+    autoStyleContainer: false
+  },
+  from: {color: '#ED1B23'},
+  to: {color: '#10B04B'},
+  step: (state, bar) => {
+    bar.setText(Math.round(bar.value() * 100) + ' %');
+  }
+});
+
+bar.animate(1.0);  // Number from 0.0 to 1.0
+  </script>
+
+</body>
+
+</html>
---
+++ js/demo/chart-pie-gln.js
@@ -0,0 +1,35 @@
+// Set new default font family and font color to mimic Bootstrap's default styling
+Chart.defaults.global.defaultFontFamily = 'Nunito', '-apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif';
+Chart.defaults.global.defaultFontColor = '#858796';
+
+// Pie Chart Example
+var ctx = document.getElementById("myPieChart");
+var myPieChart = new Chart(ctx, {
+  type: 'doughnut',
+  data: {
+    labels: ["Vitoria Stone Fair", "Cachoeiro Stone Fair", "Mec Show", "Expo Construções", "Wine South America"],
+    datasets: [{
+      data: [38, 19, 17, 4, 22,],
+      backgroundColor: ['#964097', '#40BDED', '#61C086', '#A3CC3F', '#CFDD26'],
+      hoverBackgroundColor: ['#A765A9', '#70CAF1', '#85CB9D', '#B6D365', '#DAE25A'],
+      hoverBorderColor: "rgba(234, 236, 244, 1)",
+    }],
+  },
+  options: {
+    maintainAspectRatio: false,
+    tooltips: {
+      backgroundColor: "rgb(255,255,255)",
+      bodyFontColor: "#858796",
+      borderColor: '#dddfeb',
+      borderWidth: 1,
+      xPadding: 15,
+      yPadding: 15,
+      displayColors: false,
+      caretPadding: 10,
+    },
+    legend: {
+      display: false
+    },
+    cutoutPercentage: 80,
+  },
+});
